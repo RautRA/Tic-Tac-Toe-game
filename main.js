8cf6c34f1dcf4011c0271e22e57ff18485149165
RautRA <168980384+RautRA@users.noreply.github.com>
2024-09-05 19:33:35 +0530

    Refactored the code to make the board one dimensional array for simplicity

--- main.js
+++ main.js
@@ -1,21 +1,16 @@
 const GameBoard = (function() {
-    const rows = 3;
-    const columns = 3;
     const board = [];
 
-    for(let i = 0; i < rows; i++) {
-        board[i] = [];
-        for(let j = 0; j < columns; j++) {
-            board[i].push(null);
-        }
+    for(let i = 0; i < 9; i++) {
+        board.push(null);
     }
 
     const getBoard = function() {
         return board;
     };
 
-    const addMarker = function(row, column, marker) {
-        board[row][column] = marker;
+    const addMarker = function(pos, marker) {
+        board[pos] = marker;
         return board;
     }
 
@@ -42,23 +37,19 @@ const GameController = (function (playerOneName, playerTwoName, playerOneMarker,
         currentPlayerTurn = currentPlayerTurn === player1 ? player2 : player1;
     };
 
-    const getCurrentPlayerTurn = function() {
-        return currentPlayerTurn;
-    };
-
     const printNewRound = function() {
         console.log(board);
-        console.log(`${getCurrentPlayerTurn().name}'s turn.`);
+        console.log(`${currentPlayerTurn.name}'s turn.`);
         playRound();
     };
 
     const playRound = function() {
-        const [playerChoiceRow, playerChoiceColumn] = prompt(`It's ${getCurrentPlayerTurn().name}'s turn! Please specify the row & column no. where you'd like to place your marker?`).split(",");
-        if (board[playerChoiceRow][playerChoiceColumn] !== null) {
+        const playerChoice = prompt(`It's ${currentPlayerTurn.name}'s turn! Please choose the position where you'd like to place your marker?`);
+        if (board[playerChoice] !== null) {
             console.log("Invalid move! That cell is already taken please select an empty cell.");
             printNewRound();
         } else {
-            GameBoard.addMarker(playerChoiceRow, playerChoiceColumn, getCurrentPlayerTurn().marker);
+            GameBoard.addMarker(playerChoice, currentPlayerTurn.marker);
             switchPlayerTurn();
             printNewRound();
         }
